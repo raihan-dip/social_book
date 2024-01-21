4e9c4ca15062681be6bdc2884ad9635fdee5eeda
raihan-dip <79162559+raihan-dip@users.noreply.github.com>
2024-01-21 13:25:22 +0600

    4th commit

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+import uuid
+from datetime import datetime
 User = get_user_model()
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     id_user = models.IntegerField() 
     bio = models.TextField(blank = True)
-    profileimg = models.ImageField(upload_to='profile_images', default=' media/blank-profile.jpg')
+    profileimg = models.ImageField(upload_to='profile_images', default='media/blank-profile.jpg')
     location = models.CharField(max_length= 100, blank = True)
     
     
@@ -15,3 +16,13 @@ class Profile(models.Model):
         return self.user.username
         
     
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
